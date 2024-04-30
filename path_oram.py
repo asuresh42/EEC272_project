a147b4c683ad3e19f9c5caedc7231ddfc299249f
asuresh42 <146984888+asuresh42@users.noreply.github.com>
2024-04-29 23:59:42 -0700

    Update path_oram.py
    Added some skeleton methods and comments

--- path_oram.py
+++ path_oram.py
@@ -4,11 +4,27 @@ class p_oram:
     def __init__(self, L = 3, Z = 1, C = 8):
         default_bucket = [{"valid": 0, "dummy": 0, "block_num":0, "RAM_address":0}]
 
+        #Initialize binary tree
         self.btree = binary_tree.BinaryTree(default_bucket)
         self.btree.root = self.btree.create_full_binary_tree(L+1, default_bucket)
 
+        #Initialize Pos Map
+
+
+        #Initialize Stash
+
     def __print_btree__(self):
         self.btree.inorder_traversal(self.btree.root)
+
+    def ORAM_access(block_num):
+        #Use Pos Map to determine leaf / path
+        #Pop entire path from Binary tree and store in stash
+        #Retrieve bucket / block of interest and get RAM address
+        #Retrieve data from ORAM using actual address
+        #Return data to client
+
+    def periodic_stash_eviction():
+        pass
 
 
 
