e3b11210990abf1e27ce13ad1a984c16c89b37ea
asuresh42 <146984888+asuresh42@users.noreply.github.com>
2024-04-30 16:49:15 -0700

    Update path_oram.py
    Added a basic ORAM_write.
    Fixed some bugs with older code
    Added a basic test (still in progress)

--- path_oram.py
+++ path_oram.py
@@ -1,17 +1,20 @@
 import binary_tree
 from queue import Queue
 import os
+import random
 
-def generate_random_data(byte_size):
+def generate_random_data(byte_size): #generate_random_data(self.Block_byte_size)
     return os.urandom(byte_size)
 
 class p_oram:
     def __init__(self, L = 3, Z = 1, C = 8, B=8):
-        default_bucket = [{"valid": 0, "dummy": 0, "block_num":0, "RAM_address":0}]
+        self.default_bucket = {"valid": 0, "notdummy": 0, "block_num":0, "Data":0}
 
         #Initialize binary tree
-        self.btree = binary_tree.BinaryTree(default_bucket)
-        self.btree.root = self.btree.create_full_binary_tree(L+1, default_bucket)
+        self.btree = binary_tree.BinaryTree(self.default_bucket)
+        self.btree.root = self.btree.create_full_binary_tree(L+1, self.default_bucket)
+        self.btree.find_paths_to_leaves()
+        self.leaves_list = list(self.btree.leaf_paths.keys())
 
         #Initialize Pos Map
         self.pos_map = {} #key is block_num and value is node ID of leaf
@@ -24,12 +27,11 @@ class p_oram:
     def print_btree(self):
         self.btree.inorder_traversal(self.btree.root)
 
-    def ORAM_access(self, block_num):
+    def ORAM_read(self, block_num):
         #Client provides block_num for required data block (in RAM)
 
         #Use Pos Map to determine leaf / path to check
         leaf_id = self.pos_map[block_num]
-        self.btree.find_paths_to_leaves
         leaf_path = self.btree.leaf_paths[leaf_id]
 
         #Pop entire path from Binary tree and store in stash
@@ -41,7 +43,8 @@ class p_oram:
                 client_block = block
         
         #Retrieve data from ORAM using actual address
-        block_data = generate_random_data(self.Block_byte_size)
+        block_data = block["Data"]
+        
         #Return data to client
         return block_data
 
@@ -50,10 +53,46 @@ class p_oram:
         for i in path:
             if(node.data["vaid"]):
                 self.stash.append(node.data)
+                node.data = self.default_bucket
             if(i):
                 node = node.right
             else:
                 node = node.left
+
+    def ORAM_write(self, block_num, data):
+        #generate random leaf choice
+        leaf_candidate = random.choice(self.leaves_list)
+        status = self.add_to_btree(node=self.btree.root, data=data, block_num=block_num, notdummy=1, random_leaf_path=self.btree.leaf_paths[leaf_candidate])
+        if(status["data_placed"]):
+            print(f"Successfully placed data in path to leaf {leaf_candidate}")
+            self.pos_map[block_num] = leaf_candidate
+        else:
+            print("Failed to place data")
+
+
+    def add_to_btree(self, node, data, block_num, notdummy, random_leaf_path):
+        if(len(random_leaf_path)):
+            if(random_leaf_path[0]):
+                status = self.add_to_btree(node=node.right, data=data, block_num=block_num, notdummy=notdummy, random_leaf_path=random_leaf_path[1:])
+            else:
+                status = self.add_to_btree(node=node.left, data=data, block_num=block_num, notdummy=notdummy, random_leaf_path=random_leaf_path[1:])
+            
+            if(status["data_placed"]):
+                    return status
+            else:
+                if(node.data["valid"]):
+                    return {"node":node, "data_placed":0}
+                else:
+                    node.data = {"valid": 1, "notdummy": notdummy, "block_num":block_num, "Data":data}
+                    return {"node":node, "data_placed":1}
+        
+        #is current node a leaf
+        if(node.data["valid"]):
+            return {"node":node, "data_placed":0}
+        else:
+            node.data = {"valid": 1, "notdummy": notdummy, "block_num":block_num, "Data":data}
+            return {"node":node, "data_placed":1}
+
 
     def periodic_stash_eviction(self):
         return ""
@@ -65,6 +104,12 @@ class p_oram:
 # Example usage
 if __name__ == '__main__':
  poram_instance = p_oram()
- poram_instance.__print_btree__()
+ poram_instance.print_btree()
+
+ #add a few entries into ORAM
+ random_data = generate_random_data(8)
+ poram_instance.ORAM_write(69, random_data)
+
+ poram_instance.print_btree()
 
 
